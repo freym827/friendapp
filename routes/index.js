4c2a470152b26439d1a5ccce704fdb33523a68b6
Bitter Starglow <freym827@gmail.com>
2019-02-03 20:53:23 -0500

    sutff

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection')
 module.exports = function(app) {
     app.get('/api/friends', (req, res) => {
         connection.query('SELECT * FROM friends', (err, data) => {
-            err?res.send(err):res.json({locations: data})
+            err?res.send(err):res.json({friends: data})
         })
     })
 
