c58ae8b6e20b4bb621a54ea62847525024cbf0c8
Bitter Starglow <freym827@gmail.com>
2019-02-03 19:57:40 -0500

    post route

--- public/assets/app.js
+++ public/assets/app.js
@@ -8,4 +8,29 @@ $(() => {
               $("#resp").html(response.locations[0].name)
           });
     })
+    $("#add").on("click", () => {
+        const newperson = {
+            name: $("#name").val(),
+            photo: $("#photo").val(),
+            s1: $("#q1").val(),
+            s2: $("#q2").val(),
+            s3: $("#q3").val(),
+            s4: $("#q4").val(),
+            s5: $("#q5").val(),
+            s6: $("#q6").val(),
+            s7: $("#q7").val(),
+            s8: $("#q8").val(),
+            s9: $("#q9").val(),
+            s10: $("#q10").val()
+        }
+        $.ajax("/api/friends", {
+            type: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify(newperson)
+          }).then(response => {
+            console.log(response)
+          });
+    })
 }) 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const connection = require('../config/connection')
-
 module.exports = function(app) {
     app.get('/api/friends', (req, res) => {
         connection.query('SELECT * FROM friends', (err, data) => {
             err?res.send(err):res.json({locations: data})
         })
     })
+
+    app.post('/api/friends', (req, res) => {
+        connection.query("INSERT INTO friends (name, photo, score_1, score_2, score_3, score_4, score_5, score_6, score_7, score_8, score_9, score_10) " + 
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+         [req.body.name, req.body.photo, req.body.s1, req.body.s2, req.body.s3, req.body.s4, req.body.s5, req.body.s6, req.body.s7, req.body.s8, req.body.s9, req.body.s10], 
+         (err, data) => {
+            if(err) {
+                console.log(err)
+            }else {
+                console.log(data)
+            }
+        });
+    });
 }
